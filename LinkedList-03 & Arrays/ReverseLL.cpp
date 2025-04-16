628e2283ef80162711f4ddee2f504a0503a36347
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-04-16 23:59:38 +0530

    Reverse Linked List

---
+++ LinkedList-03 & Arrays/ReverseLL.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* reverseList(Node* head) {
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next; // Store next node
+        curr->next = prev; // Reverse the link
+        prev = curr;      // Move prev to current node
+        curr = next;      // Move to next node
+    }
+    return prev; // New head of the reversed list
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
