465766b87af1bf677ad8fee83549ef8de5418e06
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-07-31 23:42:53 +0530

    All DFS Traversal in Trees

---
+++ 11_Trees/2_DFS.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void preOrder(Node* root) {
+    if (root == nullptr) return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(Node* root) {
+    if (root == nullptr) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+void inOrder(Node* root) {
+    if (root == nullptr) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "PreOrder Traversal: ";
+    preOrder(root);
+    cout << endl;
+
+    cout << "PostOrder Traversal: ";
+    postOrder(root);
+    cout << endl;
+
+    cout << "InOrder Traversal: ";
+    inOrder(root);
+    cout << endl;
+
+    return 0;
+}
