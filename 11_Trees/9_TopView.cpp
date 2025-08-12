3808755cfbd7d347dfc7868d43389aff89705f65
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-08-12 22:52:14 +0530

    Top Views in Binary Trees

---
+++ 11_Trees/9_TopView.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+// To find the leftmost and rightmost positions
+void find(Node* root, int pos, int &left, int &right) {
+    if (root == nullptr) return;
+
+    // Update the leftmost and rightmost positions
+    left = min(left, pos);
+    right = max(right, pos);
+
+    // Recur for left and right subtrees
+    find(root->left, pos - 1, left, right);
+    find(root->right, pos + 1, left, right);
+}
+
+vector<int> TopView(Node* root){
+    int left = 0 , right = 0;
+    find(root, 0, left, right);
+
+    vector<int> ans (right - left + 1);
+    vector<bool> filled (right - left + 1, 0);
+    queue<Node*>q;
+    queue<int> index;
+    q.push(root);
+    index.push(-left * left);
+
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+        int pos = index.front();
+        index.pop();
+
+        // If not filled then fill the position
+        if(!filled[pos]){
+            filled[pos]=1;
+            ans[pos] = temp->data;
+        }
+
+        // Push left child
+        if(temp->left){
+            q.push(temp->left);
+            index.push(pos - 1);
+        }
+        // Push right child
+        if(temp->right){
+            q.push(temp->right);
+            index.push(pos + 1);
+        }
+    }
+
+    return ans;
+}
+
+int main (){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+    root->right->right->right = new Node(7);
+
+    vector<int> topView = TopView(root);
+    for (int val : topView) {
+        cout << val << " ";
+    }
+    return 0;
+}
---
+++ 11_Trees/9_TopViewRecursion.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void find(Node* root, int pos, int &left, int &right) {
+    if (!root) return;
+    left = min(left, pos);
+    right = max(right, pos);
+    find(root->left, pos - 1, left, right);
+    find(root->right, pos + 1, left, right);
+}
+
+// Recursive function to fill top view
+void Tview(Node* root, int pos, vector<int> &ans, vector<int> &level, int depth) {
+    if (!root) return;
+
+    // If first time visiting this horizontal distance or at smaller depth
+    if (depth < level[pos]) {
+        level[pos] = depth;
+        ans[pos] = root->data;
+    }
+
+    Tview(root->left, pos - 1, ans, level, depth + 1);
+    Tview(root->right, pos + 1, ans, level, depth + 1);
+}
+
+vector<int> TopView(Node* root) {
+    int left = 0, right = 0;
+    find(root, 0, left, right);
+
+    vector<int> ans(right - left + 1);
+    vector<int> level(right - left + 1, INT_MAX);
+
+    Tview(root, -left, ans, level, 0);
+    return ans;
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+    root->right->right->right = new Node(7);
+
+    vector<int> topView = TopView(root);
+    for (int val : topView) {
+        cout << val << " ";
+    }
+    return 0;
+}
